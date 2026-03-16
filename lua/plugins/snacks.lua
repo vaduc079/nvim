527baaadf75f1cb0ac8168059b5fbe948db9736c
Duc Vu <duc.vu@shopback.com>
2026-03-16 12:32:59 +0700

    feat: init

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,29 @@
+return {
+  "folke/snacks.nvim",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        if Snacks.picker.get({ source = "explorer" })[1] == nil then
+          Snacks.picker.explorer()
+        elseif Snacks.picker.get({ source = "explorer" })[1]:is_focused() == true then
+          Snacks.picker.explorer()
+        elseif Snacks.picker.get({ source = "explorer" })[1]:is_focused() == false then
+          Snacks.picker.get({ source = "explorer" })[1]:focus()
+        end
+      end,
+      desc = "Explorer Snacks (root dir)",
+    },
+  },
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          layout = { layout = { position = "right" } },
+          hidden = true,
+        },
+        files = { hidden = true },
+      },
+    },
+  },
+}

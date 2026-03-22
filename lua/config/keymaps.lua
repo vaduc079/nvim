6761e8a7e47d36b65f2a5902bb883936453e94e4
Duc Vu <duc.vu@shopback.com>
2026-03-22 17:48:16 +0700

    feat(wezterm-move): add wezterm-move

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.set("n", "<C-h>", function()
+  require("wezterm-move").move("h")
+end, { desc = "Go to Left Window" })
+vim.keymap.set("n", "<C-j>", function()
+  require("wezterm-move").move("j")
+end, { desc = "Go to Lower Window" })
+vim.keymap.set("n", "<C-k>", function()
+  require("wezterm-move").move("k")
+end, { desc = "Go to Upper Window" })
+vim.keymap.set("n", "<C-l>", function()
+  require("wezterm-move").move("l")
+end, { desc = "Go to Right Window", remap = true })

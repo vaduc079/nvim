cf4b8a3b5a7c19f4129ebaa16282a8a3837e1676
Duc Vu <duc.vu@shopback.com>
2026-05-14 23:08:10 +0700

    feat: add typescript-tools.lua

---
+++ lua/plugins/typescript-tools.lua
@@ -0,0 +1,60 @@
+local ts_filetypes = {
+  "javascript",
+  "javascriptreact",
+  "javascript.jsx",
+  "typescript",
+  "typescriptreact",
+  "typescript.tsx",
+}
+
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "typescript-language-server",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = { enabled = false },
+        ts_ls = { enabled = false },
+        tsserver = { enabled = false },
+        tsgo = { enabled = false },
+      },
+    },
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    ft = ts_filetypes,
+    keys = {
+      { "gD", "<cmd>TSToolsGoToSourceDefinition<cr>", desc = "Goto Source Definition", ft = ts_filetypes },
+      { "gR", "<cmd>TSToolsFileReferences<cr>", desc = "File References", ft = ts_filetypes },
+      { "<leader>cM", "<cmd>TSToolsAddMissingImports<cr>", desc = "Add missing imports", ft = ts_filetypes },
+      { "<leader>cD", "<cmd>TSToolsFixAll<cr>", desc = "Fix all diagnostics", ft = ts_filetypes },
+      { "<leader>co", "<cmd>TSToolsOrganizeImports<cr>", desc = "Organize Imports", ft = ts_filetypes },
+    },
+    opts = {
+      settings = {
+        complete_function_calls = true,
+        tsserver_file_preferences = {
+          includeCompletionsForModuleExports = true,
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "literals",
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = false,
+          quotePreference = "auto",
+        },
+      },
+    },
+  },
+}
